f6b1a8061a7675c91cb14e07b5e374119eb53613
B <buherator@silentsignal.hu>
2013-10-21 21:17:53 +0200

    som cleanup trying to add more granuality

--- duncan.py
+++ duncan.py
@@ -1,5 +1,4 @@
 import threading
-import time
 import requests
 import argparse
 import Queue
@@ -7,6 +6,7 @@ import Queue
 class Duncan(threading.Thread):
 	def __init__(self,query='select version()',pos=1,q=None,ascii_begin=32,ascii_end=123,debug=0):
 		"""Main constructor
+
 		Keyword arguments:
         query -- Query to be executed
         pos -- Character position to test
@@ -41,7 +41,7 @@ class Duncan(threading.Thread):
 			q.put((self._pos,chr(self._max)))
 
 
-	def decide(self,guess,less_than_guess=True):
+	def decide(self,guess):
 		"""Here you should implement your injection code.
 
 		Arguments:
@@ -58,21 +58,30 @@ class Duncan(threading.Thread):
 		else:
 			return False
 
+# From http://stackoverflow.com/questions/312443/how-do-you-split-a-list-into-evenly-sized-chunks-in-python
+def chunks(l, n):
+    """ Yield successive n-sized chunks from l.
+    """
+    for i in xrange(0, len(l), n):
+		yield l[i:i+n]
+
 parser = argparse.ArgumentParser(description='Duncan - Blind SQL injector skeleton')
 parser.add_argument("--query",required=True,help="The query to be run. Should contain only one attribute.")
 parser.add_argument("--pos-start",default=1,type=int,help="First character position to look up")
 parser.add_argument("--pos-end",default=5,type=int,help="Last character position to look up")
 parser.add_argument("--ascii-start",default=32,type=int,help="Start of the ASCII range to test")
 parser.add_argument("--ascii-end",default=123,type=int,help="End of the ASCII range to test")
+parser.add_argument("--threads-per-position",default=1,type=int,help="Threads per position [EXPERIMENTAL]")
 parser.add_argument("--debug",default=0,type=int,help="Debug - higher values for more verbosity")
 
 args = parser.parse_args()
 q=Queue.Queue()
 threads=[]
 for p in xrange(args.pos_start,args.pos_end):
-	thread=Duncan(args.query,p,q,args.ascii_start,args.ascii_end,args.debug)
-	threads.append(thread)
-	thread.start()
+	for chunk in chunks(range(args.ascii_start,args.ascii_end),(args.ascii_end-args.ascii_start+1)/args.threads_per_position):
+		thread=Duncan(args.query,p,q,chunk[0],chunk[-1]+1,args.debug)
+		threads.append(thread)
+		thread.start()
 
 for t in threads:
 	t.join()
